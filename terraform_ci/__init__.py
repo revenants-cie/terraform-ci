0997ccf0afec694ec9cc008f52bf03d19646886c
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2019-12-27 11:02:35 -0800

    Bump version: 0.9.0 → 0.10.0

--- terraform_ci/__init__.py
+++ terraform_ci/__init__.py
@@ -13,7 +13,7 @@ from urllib.parse import urlparse
 import boto3
 import hcl
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 DEFAULT_TERRAFORM_VARS = ".env/tf_env.json"
 LOG = logging.getLogger(__name__)
