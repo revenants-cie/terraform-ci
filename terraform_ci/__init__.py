73be8b5b385849182519d35e75cad921ab3ac081
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2020-01-01 15:29:59 -0500

    Print a check-in message when executing a long running process
    When a process runs longer than 10 minutes travis-ci times out and kills the process.
    To prevent that print a log message every 10 seconds.

--- terraform_ci/__init__.py
+++ terraform_ci/__init__.py
@@ -2,6 +2,7 @@
 import json
 import logging
 import sys
+import time
 from contextlib import contextmanager
 from glob import glob
 from os import environ, path as osp
@@ -16,6 +17,7 @@ import hcl
 __version__ = "0.10.0"
 
 DEFAULT_TERRAFORM_VARS = ".env/tf_env.json"
+DEFAULT_PROGRESS_INTERVAL = 10
 LOG = logging.getLogger(__name__)
 
 
@@ -238,7 +240,7 @@ def run_job(path, action):
     return status
 
 
-def execute(cmd, stdout=PIPE, stderr=PIPE, cwd=None):
+def execute(cmd, stdout=PIPE, stderr=PIPE, cwd=None, progress_interval=DEFAULT_PROGRESS_INTERVAL):
     """
     Execute a command and return a tuple with return code, STDOUT and STDERR.
 
@@ -250,11 +252,22 @@ def execute(cmd, stdout=PIPE, stderr=PIPE, cwd=None):
     :type stderr: int, None
     :param cwd: Working directory.
     :type cwd: str
+    :param progress_interval: Print a message every this many seconds to give a user feedback.
+    :type progress_interval: int
     :return: Tuple (return code, STDOUT, STDERR)
     :rtype: tuple
     """
     LOG.info("Executing: %s", " ".join(cmd))
     proc = Popen(cmd, stdout=stdout, stderr=stderr, cwd=cwd)
+    last_checking = time.time()
+    while True:
+        if proc.poll() is not None:
+            break
+        if time.time() - last_checking > progress_interval:
+            LOG.info("Still waiting for process to complete.")
+            last_checking = time.time()
+        time.sleep(1)
+
     cout, cerr = proc.communicate()
     return proc.returncode, cout, cerr
 
